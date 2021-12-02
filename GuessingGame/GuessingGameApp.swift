e4f94acfd6c5f62acd8d2b18a5572575fd335577
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-02 09:09:15 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Madison Dellamea on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
