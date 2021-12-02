eb8a45d96ab2a7e0ff426e223dfca1f6f2ff5190
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-02 09:28:39 -0500

    Used String interpolation to make text show user's "currentGuess" value on screen.

--- GuessingGame/ContentView.swift
+++ GuessingGame/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Stored Properties
+    // The current guess of the user.
+    
+    @State var currentGuess: Double = 50.0
+    
+    //MARK: Computed Properties
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            Slider(value: $currentGuess,
+                   in: 0.0...100.0,
+                   label: {
+                        Text("Guess")
+            },
+                   minimumValueLabel: {
+                        Text("0")
+            },
+                   maximumValueLabel: {
+                        Text("100")
+            })
+            
+            
+            //output
+            Text("\(String(format: "%.2f", currentGuess))")
+                .font(.largeTitle)
+                .bold()
+            
+            Button(action: {
+                // NOTE: Output will not be shown unless this app is run in the "full" simulator
+                print("Button was pressed")
+            }, label: {
+                Text("Submit Guess")
+            })
+            .buttonStyle(.bordered)
+            
+            Spacer()
+        }
+        .navigationTitle("Guessing Game")
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
-        ContentView()
+        
+        NavigationView {
+            ContentView()
+        }
+        
     }
+    
 }
